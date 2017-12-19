a8ce7fb252c0dfc0ec1b1979cc0da8ea2012669d
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-18 20:46:15 -0500

    Create Xcode project

---
+++ itunesclient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  itunesclient
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-18.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
