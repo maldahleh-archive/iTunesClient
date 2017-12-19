ad673228b05ed2fffd1269b317d427eb66d38e0f
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-18 20:52:22 -0500

    Implement basic model types

---
+++ itunesclient/Models/Artist.swift
@@ -0,0 +1,27 @@
+//
+//  Artist.swift
+//  itunesclient
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-18.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    let id: Int
+    let name: String
+    let primaryGenre: Genre
+    var albums: [Album]
+    
+    init(id: Int, name: String, primaryGenre: Genre, albums: [Album]) {
+        self.id = id
+        self.name = name
+        self.primaryGenre = primaryGenre
+        self.albums = albums
+    }
+    
+    convenience init(id: Int, name: String, primaryGenre: Genre) {
+        self.init(id: id, name: name, primaryGenre: primaryGenre, albums: [])
+    }
+}
