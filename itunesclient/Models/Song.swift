92dd255f13bde817754db2c886abb123aa07c2e0
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-18 21:13:29 -0500

    Model Album type

---
+++ itunesclient/Models/Song.swift
@@ -0,0 +1,11 @@
+//
+//  Song.swift
+//  itunesclient
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-18.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import Foundation
+
+class Song {}
