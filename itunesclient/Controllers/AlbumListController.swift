5e8b0823882ec1bf488753eaffe7e16a6d3dd653
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-06 22:32:28 -0500

    Implement data spirce for Album List Controller

--- itunesclient/Controllers/AlbumListController.swift
+++ itunesclient/Controllers/AlbumListController.swift
@@ -16,10 +16,16 @@ class AlbumListController: UITableViewController {
     
     var artist: Artist!
     
+    lazy var dataSource: AlbumListDataSource = {
+        return AlbumListDataSource(albums: self.artist.albums)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = artist.name
+        
+        tableView.dataSource = dataSource
     }
     
     // MARK: - Table View Delegate
