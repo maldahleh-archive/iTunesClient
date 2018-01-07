c3a819df0d04312d85fb7b1522cb18ed69d92952
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-06 22:00:35 -0500

    Create custom type for album cell

---
+++ itunesclient/Views/AlbumCell.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumCell.swift
+//  itunesclient
+//
+//  Created by Mohammed Al-Dahleh on 2018-01-06.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell: UITableViewCell {
+    
+    static let reuseIdentifier = "AlbumCell"
+
+    @IBOutlet weak var artworkView: UIImageView!
+    @IBOutlet weak var albumTitleLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
