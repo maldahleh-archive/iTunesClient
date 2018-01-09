d78cb0097948a697716832893bc64a4d6b274736
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-08 22:45:02 -0500

    Create album list controller

---
+++ itunesclient/Controllers/AlbumDetailController.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumDetailController.swift
+//  itunesclient
+//
+//  Created by Mohammed Al-Dahleh on 2018-01-08.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDetailController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
