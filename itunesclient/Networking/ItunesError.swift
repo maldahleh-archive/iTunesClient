4d7278a786450f9abeb4dda542c5c7ccbb6cdf20
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-15 21:43:15 -0500

    JSON downloading and object initialization from JSON

---
+++ itunesclient/Networking/ItunesError.swift
@@ -0,0 +1,16 @@
+//
+//  ItunesError.swift
+//  itunesclient
+//
+//  Created by Mohammed Al-Dahleh on 2018-01-15.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import Foundation
+
+enum ItunesError: Error {
+    case requestFailed
+    case responseUnsuccessful
+    case invalidData
+    case jsonConversionFailure
+}
---
+++ itunesclient/Networking/JSONDownloader.swift
@@ -0,0 +1,47 @@
+//
+//  JSONDownloader.swift
+//  itunesclient
+//
+//  Created by Mohammed Al-Dahleh on 2018-01-15.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import Foundation
+
+typealias JSON = [String: AnyObject]
+typealias JSONTaskCompletionHandler = (JSON?, ItunesError?) -> Void
+
+class JSONDownloader {
+    let session: URLSession
+    
+    init() {
+        session = URLSession(configuration: .default)
+    }
+    
+    func jsonTask(with request: URLRequest, completionHandler completion: @escaping JSONTaskCompletionHandler) -> URLSessionDataTask {
+        let task = session.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(nil, .requestFailed)
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? JSON
+                        completion(json, nil)
+                    } catch {
+                        completion(nil, .jsonConversionFailure)
+                    }
+                } else {
+                    completion(nil, .invalidData)
+                }
+            } else {
+                completion(nil, .responseUnsuccessful)
+            }
+            
+        }
+        
+        return task
+    }
+}
